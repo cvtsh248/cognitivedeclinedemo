cfac8837cc5038dfcd5f44666a09456bf17d82a0
cvtsh248 <cvtsh248@gmail.com>
2025-11-16 00:07:09 +0800

    got started on non ml lexical parameters

--- src/lib/nlp_helper.py
+++ src/lib/nlp_helper.py
@@ -1,1 +1,26 @@
 import nltk
+# from nltk.corpus import words
+
+# lexical parameters
+def type_token_ratio(phrase: str) -> float:
+    tokenized = nltk.tokenize.word_tokenize(phrase)
+    if len(tokenized) > 0:
+        return len(set(tokenized))/len(tokenized)
+    else:
+        raise Exception("No tokens detected, input phrase is probably empy")
+    
+def lexical_density_halliday(phrase: str) -> float:
+    LEXICAL_TAGS = {
+        "NN", "NNS", "NNP", "NNPS",
+        "VB", "VBD", "VBG", "VBN", "VBP", "VBZ",
+        "JJ", "JJR", "JJS",
+        "RB", "RBR", "RBS"
+    }
+    tokenized = nltk.tokenize.word_tokenize(phrase)
+    tagged = nltk.tag.pos_tag(tokenized)
+
+    if len(tokenized) > 0:
+        return len([token for token, tag in tagged if tag in LEXICAL_TAGS])/len(tokenized)
+    else:
+        raise Exception("No tokens detected, input phrase is probably empy")
+    
